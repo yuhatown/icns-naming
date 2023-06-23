4c1d96a647f786c85cd34e0f402fcae87959e96e
taewan <yuhatown@gmail.com>
2023-06-23 12:23:18 +0900

    update testcode

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,43 +1,49 @@
-import { bech32ToICNS, ICNSToBech32 } from '../src/index';  
-import fetchMock from 'jest-fetch-mock';
+import { bech32ToICNS, ICNSToBech32 } from "../src/index";
+import fetchMock from "jest-fetch-mock";
 
 afterEach(() => {
-    fetchMock.resetMocks();
-  });
+  fetchMock.resetMocks();
+});
 
-describe('bech32ToICNS', () => {
-  it('should return the correct name', async () => {
+describe("bech32ToICNS", () => {
+  it("should return the correct name", async () => {
     const mockResponse = {
       data: {
-        name: 'dogemos.osmo'
-      }
+        name: "dogemos.osmo",
+      },
     };
-    const bech32_address = 'osmo1z98eg2ztdp2glyla62629nrlvczg8s7f8sgpm5'
-    const resolver_address = 'osmo1xk0s8xgktn9x5vwcgtjdxqzadg88fgn33p8u9cnpdxwemvxscvast52cdd'
-
+    const bech32_address = "osmo1z98eg2ztdp2glyla62629nrlvczg8s7f8sgpm5";
+    const resolver_address =
+      "osmo1xk0s8xgktn9x5vwcgtjdxqzadg88fgn33p8u9cnpdxwemvxscvast52cdd";
+    const baseUrl = "https://lcd-osmosis.keplr.app/cosmwasm/wasm/v1/contract/";
 
     fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
-    const result = await bech32ToICNS(bech32_address, resolver_address);
+    const result = await bech32ToICNS(
+      baseUrl,
+      bech32_address,
+      resolver_address
+    );
     console.log(result);
-    
-    expect(result).toEqual('dogemos.osmo');
+
+    expect(result).toEqual("dogemos.osmo");
   });
 });
 
-describe('ICNSToBech32', () => {
-  it('should return the correct address', async () => {
+describe("ICNSToBech32", () => {
+  it("should return the correct address", async () => {
     const mockResponse = {
       data: {
-        bech32_address: 'osmo1z98eg2ztdp2glyla62629nrlvczg8s7f8sgpm5'
-      }
+        bech32_address: "osmo1z98eg2ztdp2glyla62629nrlvczg8s7f8sgpm5",
+      },
     };
-    const icns_address = 'dogemos.osmo'
-    const resolver_address = 'osmo1xk0s8xgktn9x5vwcgtjdxqzadg88fgn33p8u9cnpdxwemvxscvast52cdd'
-
+    const icns_address = "dogemos.osmo";
+    const resolver_address =
+      "osmo1xk0s8xgktn9x5vwcgtjdxqzadg88fgn33p8u9cnpdxwemvxscvast52cdd";
+    const baseUrl = "https://lcd-osmosis.keplr.app/cosmwasm/wasm/v1/contract/";
 
     fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
-    const result = await ICNSToBech32(icns_address, resolver_address);
-    expect(result).toEqual('osmo1z98eg2ztdp2glyla62629nrlvczg8s7f8sgpm5');
+    const result = await ICNSToBech32(baseUrl, icns_address, resolver_address);
+    expect(result).toEqual("osmo1z98eg2ztdp2glyla62629nrlvczg8s7f8sgpm5");
   });
 });
 
